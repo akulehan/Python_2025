02de5c05a5b6d20b6e07d6da662228849681db75
sholehan <akulehan@gmail.com>
2025-05-09 16:43:58 +0700

    Menambahkan file def fungsi linear

---
+++ def_fungsi_linear.py
@@ -0,0 +1,16 @@
+print("Kita akan menghitung nilai  Y dari fungsi linear")
+print("Diketahui fungsi linear Y = 2x + 3")
+def fungsi_linear1(x):
+    print("Jika x =" + str(x) + ", maka y = 2x + 3= " + str(2*x + 3))
+
+fungsi_linear1(1)
+fungsi_linear1(2)
+fungsi_linear1(3)
+fungsi_linear1(4)
+fungsi_linear1(5)
+print("Coba masukkan nilai x yang lain, kita akan menghitung nilai y")
+x = int(input("Masukkan nilai x: "))
+fungsi_linear1(x)
+#pembuatan kode ini dibantu oleh ChatGPT melalui visual studio code yang berjalan di linux
+
+
