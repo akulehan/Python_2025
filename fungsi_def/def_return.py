d48fbd8386cb315453438707af774b73ce0220a7
sholehan <akulehan@gmail.com>
2025-05-09 21:23:38 +0700

    Perbaikan struktur fungsi def hendaknya didefinisikan dulu, baru ditentukan nilai variabelnya

--- fungsi_def/def_return.py
+++ fungsi_def/def_return.py
@@ -1,12 +1,12 @@
 print ('\n')
 print('Kita akan bantu menghitung sisa bagi')
-bilangan = int(input('Masukkan bilangan yang akan dibagi: '))
-pembagi = int(input('Masukkan bilangan pembagi: '))
 def pembagian (bilangan,pembagi):
     hasilbagi = bilangan/pembagi
     hasilbagibulat =int(hasilbagi)
     sisapembagian = bilangan - (hasilbagibulat*pembagi)
     return sisapembagian
+bilangan = int(input('Masukkan bilangan yang akan dibagi: '))
+pembagi = int(input('Masukkan bilangan pembagi: '))
 print ('Modulus atau sisa bagi :' ,pembagian (bilangan,pembagi))
 print ('Dapat juga ditulis: ', pembagian (bilangan,pembagi),'( mod', pembagi ,')')
 print ('\n')
