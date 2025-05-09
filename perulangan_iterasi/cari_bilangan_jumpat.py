e041009b484028d021e13d3bca5270511ba7bf14
sholehan <akulehan@gmail.com>
2025-05-10 06:25:07 +0700

    Menambahkan file cari bilangan jumpat

---
+++ perulangan_iterasi/cari_bilangan_jumpat.py
@@ -0,0 +1,19 @@
+#membuat iterasi penjumlahan n bilangan positif pertama
+n = 1
+u = 20
+while n < u:
+    Sn=int((n*(n+1))/2)
+    print (Sn)
+    n+=1
+print ('-------------------------------------')
+print ('\n')
+#mencari nilai n yang memenuhi 4a + 6
+a = 1
+u = 20
+while a < u:
+    Ju=4*a+6
+    print (Ju)
+    a+=1
+print ('-------------------------------------')
+print ('\n')
+#memeriksa k1 yang sama dengan = k2
