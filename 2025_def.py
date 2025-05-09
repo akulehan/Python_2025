513f66d4c6a414b5cccd8f2571b0a482438d08ad
sholehan <akulehan@gmail.com>
2025-05-09 21:13:48 +0700

    Menambahkan file def umum, menghapus def yang tak berguna heheh

--- 2025_def.py
+++
@@ -1,9 +0,0 @@
-print('Hallo, nama kamu siapa? Ketikkan ya!')
-nama=input("Nama :")
-print('Terimakasih. Salam Kenal',nama,'!')
-print('Aku punya teka-teki untukmu? Apakah kamu bersedia menjawab?')
-kesediaan=input('Jawabanmu:')
-if kesediaan=='Ya' or kesediaan=='ya' or kesediaan=='YA' or kesediaan=='Y' or kesediaan=='y' or kesediaan=='mau':
-   print('Bagus, Ayo kita mulai bermain')
-else:
-   print('Baiklah lain kali kita bermain')
---
+++ def_umum.py
@@ -0,0 +1,8 @@
+def ukuran_persegi(panjang, lebar):
+    print("Luas Persegi Panjang adalah: ", panjang * lebar)
+
+#masukkan inputan panjang dan lebar
+panjang = int(input("Masukkan panjang: "))
+lebar = int(input("Masukkan lebar: "))
+ukuran_persegi(panjang, lebar)
+
