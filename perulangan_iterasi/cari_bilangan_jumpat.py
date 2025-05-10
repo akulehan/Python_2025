b95cadd7a51d09de89e589795aec05a811b2573f
sholehan <akulehan@gmail.com>
2025-05-10 13:26:39 +0700

    Masih mencari bilangan jumpat dengan logikan sendiri

--- perulangan_iterasi/cari_bilangan_jumpat.py
+++ perulangan_iterasi/cari_bilangan_jumpat.py
@@ -1,19 +1,43 @@
-#membuat iterasi penjumlahan n bilangan positif pertama
+#Suatu bilangan bulat positif n disebut bilangan JUMPAT jika jumlah n bilangan bulat positif pertama dapat dinyatakan sebagai penjumlahan empat bilangan bulat positif berurutan. Banyaknya bilangan JUMPAT yang kurang dari 2024 adalah ....
+#pencarian bilangan jumpat
+print ('\n')
 n = 1
-u = 20
+a = 1
+u = 100
+setSn = set() #setSn adalah himpunan untuk menampung Sn
+setJu = set() #juga himpunan untuk menampung Ju
 while n < u:
     Sn=int((n*(n+1))/2)
-    print (Sn)
+    setSn.add(Sn)
     n+=1
-print ('-------------------------------------')
-print ('\n')
-#mencari nilai n yang memenuhi 4a + 6
-a = 1
-u = 20
+    print(Sn, end=' ')
+print ('-----------------------------------')
 while a < u:
     Ju=4*a+6
-    print (Ju)
+    setJu.add(Ju)
     a+=1
-print ('-------------------------------------')
+    print(Ju, end=' ')
 print ('\n')
-#memeriksa k1 yang sama dengan = k2
+Jumpat = setSn & setJu #irisan 2 himpunan
+print ('Jumpat : ',Jumpat)
+print ('\n')
+print ('--------------------xxxxxxxxxxx-----------------------')
+
+#pengulangan untuk mencari bilangan jumpat 1- 2024
+print ('\n')
+n = 1
+a = 1
+u = 2024
+setSn = set()
+setJu = set()
+while n < u:
+    Sn=int((n*(n+1))/2)
+    setSn.add(Sn)
+    n+=1
+while a < u:
+    Ju=4*a+6
+    setJu.add(Ju)
+    a+=1
+Jumpat = setSn & setJu 
+jlhjumpat = len(Jumpat)
+print ('Jumlah bilangan jumpat 1-2024 : ', jlhjumpat)
