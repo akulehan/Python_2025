a84fe957806c74204f4ee81218611ec6d28a6064
sholehan <akulehan@gmail.com>
2025-05-10 17:16:49 +0700

    Cari bilangan jumpat selesai

--- perulangan_iterasi/cari_bilangan_jumpat.py
+++ perulangan_iterasi/cari_bilangan_jumpat.py
@@ -1,43 +1,10 @@
 #Suatu bilangan bulat positif n disebut bilangan JUMPAT jika jumlah n bilangan bulat positif pertama dapat dinyatakan sebagai penjumlahan empat bilangan bulat positif berurutan. Banyaknya bilangan JUMPAT yang kurang dari 2024 adalah ....
-#pencarian bilangan jumpat
-print ('\n')
-n = 1
-a = 1
-u = 100
+n = range (1,2024)
 setSn = set() #setSn adalah himpunan untuk menampung Sn
-setJu = set() #juga himpunan untuk menampung Ju
-while n < u:
-    Sn=int((n*(n+1))/2)
-    setSn.add(Sn)
-    n+=1
-    print(Sn, end=' ')
-print ('-----------------------------------')
-while a < u:
-    Ju=4*a+6
-    setJu.add(Ju)
-    a+=1
-    print(Ju, end=' ')
-print ('\n')
-Jumpat = setSn & setJu #irisan 2 himpunan
+for i in n:
+    Sn=int((i*(i+1))/2)
+    a=(Sn-6)/4
+    if a==int(a):
+        setSn.add(Sn) #menyimpan Sn ke dalam himpunan
+Jumpat = len (setSn) #menghitung banyaknya anggota setSn
 print ('Jumpat : ',Jumpat)
-print ('\n')
-print ('--------------------xxxxxxxxxxx-----------------------')
-
-#pengulangan untuk mencari bilangan jumpat 1- 2024
-print ('\n')
-n = 1
-a = 1
-u = 2024
-setSn = set()
-setJu = set()
-while n < u:
-    Sn=int((n*(n+1))/2)
-    setSn.add(Sn)
-    n+=1
-while a < u:
-    Ju=4*a+6
-    setJu.add(Ju)
-    a+=1
-Jumpat = setSn & setJu 
-jlhjumpat = len(Jumpat)
-print ('Jumlah bilangan jumpat 1-2024 : ', jlhjumpat)
